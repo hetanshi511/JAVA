1330658fb6c89e1a3bd2b42245dc2791cbffb378
hetanshi511 <hetanshipatel48@gmail.com>
2023-07-08 10:39:41 +0530

    Number

---
+++ Number.java
@@ -0,0 +1,8 @@
+class Number {
+    public static void main(String arg[]) {
+        int a = Integer.parseInt(arg[0]);
+        int b = Integer.parseInt(arg[1]);
+        int sum = a + b;
+        System.out.println("Sum of these two numbers is = " + sum);
+    }
+}
