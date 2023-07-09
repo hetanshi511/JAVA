b772f23d7f41311c32f72f9fadf0c0623b4fbdfb
hetanshi511 <hetanshipatel48@gmail.com>
2023-07-09 23:29:54 +0530

    Prime3

--- Prime3.java
+++ Prime3.java
@@ -1,8 +1,12 @@
-class Prime3 {
-    public static void main(String arg[]) {
-        int n = Integer.parseInt(arg[0]);
-        int i = 1, j = 1, count = 0, num = 0;
-        while (num <= n) {
+class Prime3{
+    public static void main(String[] args){
+int n = Integer.parseInt(args[0]);
+        int i = 1, num = 0;
+
+        while (num < n) {
+            int count = 0;
+            int j = 1;
+            
             while (j <= i) {
                 if (i % j == 0) {
                     count++;
@@ -11,7 +15,7 @@ class Prime3 {
             }
 
             if (count == 2) {
-                System.out.println("Prime Number " + num);
+                System.out.println("Prime Number " + i);
                 num++;
             }
 
